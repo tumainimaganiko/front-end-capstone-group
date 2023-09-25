7038a0634e664b753b18c345695d30597ed3cc8f
Racha OULDBABAALI <rachaould@gmail.com>
2023-09-25 15:01:13 +0100

    Add routes to the App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './styles/style.css';
+import SignUp from './pages/SignUp';
+import LogIn from './pages/LogIn';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/LogIn" element={<LogIn />} />
+      </Routes>
+    </Router>
   );
 }
 
