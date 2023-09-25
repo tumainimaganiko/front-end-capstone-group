ff1c0c80fc81eaef18462d719b5af567a1e82af5
tumainimaganiko <tumainimaganiko@gmail.com>
2023-09-25 18:01:55 +0300

    Create Navigation Menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import './App.css';
+import NavigationPanel from './components/NavigationPanel';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NavigationPanel />
   );
 }
 
---
+++ src/components/NavigationPanel.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NavigationPanel() {
+  return (
+    <div>NavigationPanel</div>
+  );
+}
+
+export default NavigationPanel;
