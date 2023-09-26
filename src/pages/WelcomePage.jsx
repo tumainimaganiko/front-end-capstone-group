03aed54a47ae05a383b528e744a72eba5d9d3308
Racha OULDBABAALI <rachaould@gmail.com>
2023-09-26 13:09:52 +0100

    Add welcome page

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,17 +1,26 @@
 import '../styles/welcome.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignInAlt, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 
 function WelcomePage() {
   return (
-    <div className="welcome flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold mb-8">Welcome to Car Rental</h1>
-      <div className="flex space-x-4">
-        <button type="button" className=" bg-blue-500 text-white rounded hover:bg-blue-600">
-          <Link to="/LogIn" className="outline_btn px-14">Login</Link>
+    <div className="welcome flex flex-col items-center ">
+      <h1 className="text-5xl font-bold mt-14  font-sans text-white uppercase "> Welcome to Car Rental</h1>
+      <p className="mb-25">Find the perfect car for your next adventure</p>
+      <div className="flex my-36 space-x-6">
+        <button type="button" className="bg-primary text-white rounded  hover:bg-lime-400 ">
+          <Link to="/LogIn" className="p-10 whitespace-nowrap ">
+            <FontAwesomeIcon icon={faSignInAlt} className="mr-2" />
+            Sign In
+          </Link>
         </button>
-        <button type="button" className=" bg-gray-500 text-white rounded hover:bg-gray-600">
-          <Link to="/SignUp" className="outline_btn px-10">Sign Up</Link>
+        <button type="button" className=" bg-primary text-white rounded hover:bg-lime-400 ">
+          <Link to="/SignUp" className="p-10 whitespace-nowrap">
+            <FontAwesomeIcon icon={faUserPlus} className="mr-2" />
+            Sign Up
+          </Link>
         </button>
       </div>
     </div>
