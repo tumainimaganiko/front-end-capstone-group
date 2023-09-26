5cfe021dca9b13b6cf7406d5192d6167c9c1f757
tumainimaganiko <tumainimaganiko@gmail.com>
2023-09-26 10:27:34 +0300

    Update Logo Position

--- src/components/NavigationPanel.jsx
+++ src/components/NavigationPanel.jsx
@@ -15,7 +15,7 @@ function NavigationPanel() {
       </button>
       {menu && (
       <nav className="flex flex-col border-r bg-white text-center md:text-left border-white md:w-[16%] lg:w-[12%] fixed md:absolute top-0 left-0 bottom-0 right-0">
-        <h2 className="pl-3 font-mono font-[900]">Car Rentals</h2>
+        <h2 className="pl-3 pt-5 font-mono font-[900]">Car Rentals</h2>
         <div className="flex flex-col w-full h-4/5 absolute md:left-0 bottom-0 md:pl-3 justify-between">
           <ul>
             <li className=""><NavLink onClick={() => setMenu(false)} className="p-2 hover:bg-[#97BF11] hover:text-white font-[900] text-xs my-2 block" to="/">ACTIVITIES</NavLink></li>
