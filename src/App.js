1708ac35ae4ff88e475570d877d183eda3f8368b
Racha OULDBABAALI <rachaould@gmail.com>
2023-09-26 13:50:42 +0100

    Add routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUp from './pages/SignUp';
 import LogIn from './pages/LogIn';
 import WelcomePage from './pages/WelcomePage';
+import Loader from './components/loader/Loader';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<WelcomePage />} />
         <Route path="/SignUP" element={<SignUp />} />
         <Route path="/LogIn" element={<LogIn />} />
+        <Route path="/Loader" element={<Loader />} />
       </Routes>
     </Router>
   );
