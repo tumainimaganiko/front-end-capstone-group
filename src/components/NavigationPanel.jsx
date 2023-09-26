c229283bbfcde540e972cf1fb15730d58dafe4c5
tumainimaganiko <tumainimaganiko@gmail.com>
2023-09-26 14:01:50 +0300

    Update Menu

--- src/components/NavigationPanel.jsx
+++ src/components/NavigationPanel.jsx
@@ -32,7 +32,7 @@ function NavigationPanel() {
         <img className=" w-1/2 h-1/5 block mx-auto" src={logo} alt="car logo" />
         <div className="flex flex-col w-full h-4/5 absolute md:left-0 bottom-0 md:pl-3 justify-between">
           <ul>
-            <li className=""><NavLink onClick={removeNavPanel} className="p-2 hover:bg-[#97BF11] hover:text-white font-[900] text-xs my-2 block" to="/">ACTIVITIES</NavLink></li>
+            <li className=""><NavLink onClick={removeNavPanel} className="p-2 hover:bg-[#97BF11] hover:text-white font-[900] text-xs my-2 block" to="/">VEHICLES</NavLink></li>
             <li className=""><NavLink onClick={removeNavPanel} className="p-2 hover:bg-[#97BF11] hover:text-white font-[900] text-xs my-2 block" to="/">RESERVATION</NavLink></li>
             <li className=""><NavLink onClick={removeNavPanel} className="p-2 hover:bg-[#97BF11] hover:text-white font-[900] text-xs my-2 block" to="/">MY RESERVATIONS</NavLink></li>
             <li className=""><NavLink onClick={removeNavPanel} className="p-2 hover:bg-[#97BF11] hover:text-white font-[900] text-xs my-2 block" to="/">ADD CAR</NavLink></li>
