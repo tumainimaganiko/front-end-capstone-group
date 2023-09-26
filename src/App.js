0d7eda5ed29dbe38fc9c39f8b6abc1fcd34174c9
tumainimaganiko <tumainimaganiko@gmail.com>
2023-09-26 09:37:21 +0300

    Add Navigation Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import {
+  Route, Outlet, Routes,
+} from 'react-router-dom';
+
 import './App.css';
 import NavigationPanel from './components/NavigationPanel';
 
+function Layout() {
+  return (
+    <>
+      <NavigationPanel />
+      <Outlet />
+    </>
+  );
+}
+
 function App() {
   return (
-    <NavigationPanel />
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<div className="text-center">dksbfe</div>} />
+      </Route>
+    </Routes>
   );
 }
 
