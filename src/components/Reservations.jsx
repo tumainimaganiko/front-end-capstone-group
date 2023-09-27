b188f8ddd33c9349789b32bfbdb9cf9893e963af
tumainimaganiko <tumainimaganiko@gmail.com>
2023-09-27 16:37:19 +0300

    Add Reseravtions Details

--- src/components/Reservations.jsx
+++ src/components/Reservations.jsx
@@ -15,9 +15,19 @@ const Reservations = () => {
           [e.target.name]: e.target.value,
         });
       };
-      
+
   return (
-    <div>Reservations</div>
+    <section>
+      <h2>RESERVE A CAR TEST-RIDE</h2>
+      <p>
+        There are 34 different versions of the Vespa. Today five series are in
+        production: the classic manual transmission PX and the modern CVT
+        transmission S: LX. GT, and GTS: We have showrooms all over the globe
+        which some include test-riding facilities. if you wish to find out if a
+        test-ride is available in your area, please use the selector below.
+        London Book Now
+      </p>
+    </section>
   )
 }
 
