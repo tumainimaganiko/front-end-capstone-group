0af014591eb1f78c675e9ad893db8c55f820a7e5
tumainimaganiko <tumainimaganiko@gmail.com>
2023-09-27 16:35:15 +0300

    Create Reservations Deatils

--- src/components/Reservations.jsx
+++ src/components/Reservations.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Reservations = () => {
+
+    const [state, setState] = useState({
+        username: '',
+        carBrand: '',
+        city: '',
+        date: '',
+      });
+      
   return (
     <div>Reservations</div>
   )
