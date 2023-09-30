5fb53a0594358efc2ed2e20d3da7c0c202612f41
tumainimaganiko <tumainimaganiko@gmail.com>
2023-09-30 14:46:25 +0300

    Fix eslint error

--- src/components/Reservations.jsx
+++ src/components/Reservations.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchReservations } from "../redux/reservations/reservationSlice";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchReservations } from '../redux/reservations/reservationSlice';
 
 function Reservations() {
   const { reservations } = useSelector((store) => store.reservations);
