534a2ac06e8b07e727247d8db87b8ece4144a9a4
tumainimaganiko <tumainimaganiko@gmail.com>
2023-09-30 14:07:38 +0300

    Update Reservation Slice

--- src/redux/reservations/reservationSlice.js
+++ src/redux/reservations/reservationSlice.js
@@ -1,19 +1,39 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
   reservations: [],
 };
+
+const baseUrl = 'http://localhost:3000/api/....';
+
+export const fetchReservations = createAsyncThunk('reservations/fetchReservations', async () => {
+  const response = await axios.get(`${baseUrl}/reservations`);
+  return response.data;
+});
+
+export const createReservation = createAsyncThunk(
+  'books/createReservation',
+  async (reservationData, thunkAPI) => {
+    const response = await axios.post(
+      `${baseUrl}/reservations/`,
+      reservationData,
+    );
+    thunkAPI.dispatch(fetchReservations());
+    return response.data;
+  },
+);
+
 const reservationSlice = createSlice({
   name: 'reservations',
   initialState,
-  reducers: {
-    createReservation: (state, action) => {
-      state.reservations.push(action.payload);
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchReservations.fulfilled, (state, action) => ({
+      ...state,
+      reservations: action.payload,
+    }));
   },
-  extraReducers: {},
 });
-
-export const { createReservation } = reservationSlice.actions;
 
 export default reservationSlice.reducer;
