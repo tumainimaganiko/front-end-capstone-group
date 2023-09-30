21ca6629f9e65a8c347583c54d8a0498ed73a9ca
tumainimaganiko <tumainimaganiko@gmail.com>
2023-09-30 14:45:41 +0300

    Create Reservations Table

--- src/components/Reservations.jsx
+++ src/components/Reservations.jsx
@@ -1,18 +1,40 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchReservations } from '../redux/reservations/reservationSlice';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchReservations } from "../redux/reservations/reservationSlice";
 
 function Reservations() {
+  const { reservations } = useSelector((store) => store.reservations);
 
-    const { reservations } = useSelector( store => store.reservations )
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchReservations());
+  }, [dispatch]);
 
-    const dispatch = useDispatch()
-    useEffect(() => {
-        dispatch(fetchReservations())
-    }, [dispatch])
-    
   return (
-    <div>Reservations</div>
+    <div>
+      <h1>Reservations</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Start Date</th>
+            <th>End Date</th>
+            <th>City</th>
+            <th>Motorcycle</th>
+          </tr>
+        </thead>
+        <tbody>
+          {reservations && reservations.map((reservation) => (
+            <tr key={reservation.id}>
+              <td>{reservation.rentDate}</td>
+              <td>{reservation.returnDate}</td>
+              <td>{reservation.city}</td>
+              <td>{reservation.car.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
