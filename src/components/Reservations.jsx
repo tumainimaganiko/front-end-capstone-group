f4798d18ab0f6fc3ecfcb41a03917ebe55afab76
tumainimaganiko <tumainimaganiko@gmail.com>
2023-09-30 14:27:46 +0300

    Retrieve Reservations

--- src/components/Reservations.jsx
+++ src/components/Reservations.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchReservations } from '../redux/reservations/reservationSlice';
 
-const Reservations = () => {
+function Reservations() {
+
+    const { reservations } = useSelector( store => store.reservations )
+
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(fetchReservations())
+    }, [dispatch])
+    
   return (
     <div>Reservations</div>
-  )
+  );
 }
 
-export default Reservations
+export default Reservations;
