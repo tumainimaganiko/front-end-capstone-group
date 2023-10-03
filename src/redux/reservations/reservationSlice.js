83a49d525034d958f2cad5cfb139b3c8794afb81
tumainimaganiko <tumainimaganiko@gmail.com>
2023-10-03 16:25:37 +0300

    Update Reservation Slice

--- src/redux/reservations/reservationSlice.js
+++ src/redux/reservations/reservationSlice.js
@@ -1,55 +1,33 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { TOKENKEY } from '../../util/auth';
 
 const initialState = {
-  reservations: [
-    {
-      rentDate: '2001',
-      returnDate: '2009',
-      city: 'Dar es Salaam',
-      car: { name: 'Mercedez Benz' },
-      amount: '$1500',
-    },
-    {
-      rentDate: '2021',
-      returnDate: '2023',
-      city: 'Niger',
-      car: { name: 'BMW' },
-      amount: '$1000',
-    },
-    {
-      rentDate: '2017',
-      returnDate: '2029',
-      city: 'Kigali',
-      car: { name: 'Porsche' },
-      amount: '$2345',
-    },
-    {
-      rentDate: '1998',
-      returnDate: '2005',
-      city: 'Algeria',
-      car: { name: 'Mc Laren' },
-      amount: '$4500',
-    },
-  ],
+  reservations: [],
 };
 
-const baseUrl = 'http://localhost:3000/api/....';
+const baseUrl = 'https://car-rental-api-91yl.onrender.com/api/v1/rentals';
 
 export const fetchReservations = createAsyncThunk('reservations/fetchReservations', async () => {
-  const response = await axios.get(`${baseUrl}/reservations`);
+  const response = await axios.get(baseUrl);
   return response.data;
 });
 
 export const createReservation = createAsyncThunk(
   'books/createReservation',
   async (reservationData, thunkAPI) => {
-    const response = await axios.post(
-      `${baseUrl}/reservations/`,
-      reservationData,
-    );
-    thunkAPI.dispatch(fetchReservations());
-    return response.data;
+    try {
+      const response = await axios.post(baseUrl, {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(localStorage.getItem(TOKENKEY))}`,
+        },
+        reservationData
+      });
+      thunkAPI.dispatch(fetchReservations());
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
   },
 );
 
