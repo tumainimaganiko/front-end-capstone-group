080fcb016de954d0aeb9b152de624a5515fc4ed7
Racha OULDBABAALI <rachaould@gmail.com>
2023-10-04 17:03:42 +0100

    Fix stylelint

--- src/test/Login.test.js
+++ src/test/Login.test.js
@@ -1,6 +1,6 @@
 import renderer from 'react-test-renderer';
 import { MemoryRouter } from 'react-router-dom';
-import Login from '../pages/Login';
+import Login from '../pages/Login.jsx';
 
 it('Render a Login', () => {
   const tree = renderer
